4d6e1ba1b95057514f60bf6ffe06135d73947f80
Se-Hun <sehunhu5247@gmail.com>
2020-11-23 14:07:40 +0900

    add parameter for input sentence num

--- data/1_build_examples.py
+++ data/1_build_examples.py
@@ -8,34 +8,61 @@ from tqdm.auto import tqdm
 
 from common.utils import prepare_dir
 
-def build_turn_change_exmaple(origin_ex):
+def build_turn_change_exmaple(origin_ex, first_sentence_num):
     id = origin_ex["id"]
     document = origin_ex["document"]
     assert (len(document) == 1), "Document has more than two data at {}".format(id)
 
     examples = []
     utterances = document[0]["utterance"]
-    for u_idx in range(0, len(utterances) - 2, 3):
-        first_utterance = utterances[u_idx]
-        second_utterance = utterances[u_idx + 1]
-        target_utterance = utterances[u_idx + 2]
+    for u_idx in range(0, (len(utterances)-first_sentence_num-1), sentence_num):
+        input_utterances, target_utterance = utterances[u_idx:(u_idx+sentence_num-1)], utterances[u_idx+sentence_num-1]
 
-        first_id = first_utterance["id"]
-        second_id = second_utterance["id"]
         target_id = target_utterance["id"]
-
-        first_text = first_utterance["form"].replace("\n", " ")
-        second_text = second_utterance["form"].replace("\n", " ")
         target_text = target_utterance["form"].replace("\n", " ")
 
-        label = 0 if second_utterance["speaker_id"] == target_utterance["speaker_id"] else 1  # speaker가 같으면 0, 틀리면 1
+        last_speaker = input_utterances[-1]["speaker_id"]
+        id_list = []
+        input_text = ""
+        for v in input_utterances:
+            text = v["form"].replace("\n", " ")
+
+            # remove data using emoticon, uploading photos ect...
+            if text == "":
+                break;
+
+            id_list.append(v["id"])
+            input_text = input_text + " " + text
 
         # remove data using emoticon, uploading photos ect...
-        if first_text == "" or second_text == "" or target_text == "":
+        if (len(id_list) != (sentence_num-1)) or target_text == "":
             continue
 
-        example = [[first_id, second_id], target_id, first_text + " " + second_text, target_text, label]
+        label = 0 if last_speaker == target_utterance["speaker_id"] else 1  # speaker가 같으면 0, 틀리면 1
+        example = [id_list, target_id, input_text, target_text, label]
         examples.append(example)
+
+    # for u_idx in range(0, len(utterances) - 2, 3):
+    #     first_utterance = utterances[u_idx]
+    #     second_utterance = utterances[u_idx + 1]
+    #     target_utterance = utterances[u_idx + 2]
+    #
+    #     first_id = first_utterance["id"]
+    #     second_id = second_utterance["id"]
+    #     target_id = target_utterance["id"]
+    #
+    #     first_text = first_utterance["form"].replace("\n", " ")
+    #     second_text = second_utterance["form"].replace("\n", " ")
+    #     target_text = target_utterance["form"].replace("\n", " ")
+    #
+    #     label = 0 if second_utterance["speaker_id"] == target_utterance["speaker_id"] else 1  # speaker가 같으면 0, 틀리면 1
+    #
+    #     # remove data using emoticon, uploading photos ect...
+    #     if first_text == "" or second_text == "" or target_text == "":
+    #         continue
+    #
+    #     example = [[first_id, second_id], target_id, first_text + " " + second_text, target_text, label]
+    #     examples.append(example)
 
     return examples
 
@@ -102,7 +129,7 @@ def buil_turn_change_example_previous(origin_ex):
 
     return examples
 
-def build_examples(fns, task, task_process_function, task_column_names):
+def build_examples(fns, task, task_process_function, task_column_names, sentence_num):
     in_fn = fns["input"]
     to_fn = fns["output"]
 
@@ -111,12 +138,12 @@ def build_examples(fns, task, task_process_function, task_column_names):
         original_data = json.load(f)
 
         # for debugging
-        N = 70
-        original_data = original_data[:N]
+        # N = 70
+        # original_data = original_data[:N]
 
         data_iterator = tqdm(original_data, desc="Iteration")
         for ex_idx, ex in enumerate(data_iterator):
-            data = data + task_process_function[task](ex)
+            data = data + task_process_function[task](ex, sentence_num)
 
     df = pd.DataFrame(data, columns=task_column_names[task])
 
@@ -128,9 +155,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--task", default="turn_change",
                         help="What task do you want ?")
+    parser.add_argument("--sentence_num", default=3, type=int,
+                        help="How many sentences use for prediction ?")
     args = parser.parse_args()
 
     task = args.task
+    sentence_num = args.sentence_num
 
     in_folder = os.path.join("./", "original-all")
     to_folder = os.path.join("./", task)
@@ -149,4 +179,4 @@ if __name__ == '__main__':
         # "turn_change": ["utterance1_ids", "utterance2_id", "label"]
     }
 
-    build_examples(fns, task, task_process_function, task_column_names)
+    build_examples(fns, task, task_process_function, task_column_names, sentence_num)
