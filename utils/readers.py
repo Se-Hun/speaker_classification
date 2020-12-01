994554559113f55fa8b997b62bc55439fc6f3e41
Se-Hun <sehunhu5247@gmail.com>
2020-12-01 10:23:21 +0900

    add functions about text_readers, tokenizer

--- utils/readers.py
+++ utils/readers.py
@@ -1,19 +1,25 @@
-from transformers import AutoModel, AutoTokenizer, BertModel
+from transformers import AutoModel, AutoTokenizer, BertForSequenceClassification
 
-def get_text_reader(reader_name):
+def get_text_reader(reader_name, num_labels):
     # Messenger Corpus is korean dataset.
     # So, model is fixed to Korean Model such as multilingual-BERT, kobert, koelectra, etc.
+
     if reader_name == "bert":
         model_name = "bert-base-multilingual-cased"
-        text_reader = AutoModel.from_pretrained(model_name)
+        # text_reader = AutoModel.from_pretrained(model_name)
+        text_reader = BertForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
 
     elif reader_name == "kobert":
-        # model_name = "monologg/kobert"
+        from transformers import BertModel
+        model_name = "monologg/kobert"
         # text_reader = BertModel.from_pretrained(model_name)
-        raise NotImplementedError("Kobert is not supported in this version.")
+        text_reader = BertForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
 
     elif reader_name == "koelectra":
-        raise NotImplementedError("Koelectra is not supported in this version.")
+        from transformers import ElectraModel, ElectraForSequenceClassification
+        model_name = "monologg/koelectra-base-discriminator"
+        # text_reader = ElectraModel.from_pretrained(model_name)
+        text_reader = ElectraForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
 
     else:
         raise KeyError(reader_name)
@@ -29,14 +35,14 @@ def get_tokenizer(reader_name):
         tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     elif reader_name == "kobert":
-        # from utils.tokenization_kobert import KoBertTokenizer
-        # tokenizer = KoBertTokenizer.from_pretrained("monologg/kobert")
-        raise NotImplementedError("Kobert is not supported in this version.")
+        from utils.tokenization_kobert import KoBertTokenizer
+        model_name = "monologg/kobert"
+        tokenizer = KoBertTokenizer.from_pretrained(model_name)
 
     elif reader_name == "koelectra":
-        # from utils.~~~ import ~~~~
-        # tokenizer = ~~~
-        raise NotImplementedError("Koelectra is not supported in this version.")
+        from transformers import ElectraTokenizer
+        model_name = "monologg/koelectra-base-discriminator"
+        tokenizer = ElectraTokenizer.from_pretrained(model_name)
 
     else:
         raise KeyError(reader_name)
